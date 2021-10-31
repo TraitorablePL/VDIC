80cd0c39479dd6fc15e160176700f3bc0721653b
Daniel Stankiewicz <traitorablepl@gmail.com>
2021-10-31 12:19:49 +0100

    Added basic test coverage for Lab02

---
+++ Lab02/alu_if.sv
@@ -0,0 +1,185 @@
+
+import alu_pkg::*;
+
+interface alu_if(
+	/* global signals */
+	output logic 	clk,
+	output logic 	rst_n,
+	
+	/* alu serial data */
+	output logic 	sin,
+	input logic 	sout
+);
+	
+	
+/**
+ * Tasks and function definitions
+ */
+
+function logic [3:0] _crc4(input logic [67:0] D);
+	
+	logic [3:0] crc;
+	
+	crc[0] = D[66] ^ D[64] ^ D[63] ^ D[60] ^ D[56] ^ D[55] ^ D[54] ^ D[53] ^ D[51] ^ D[49] ^ D[48] ^ D[45] ^ D[41] ^ D[40] ^ D[39] ^ D[38] ^ D[36] ^ D[34] ^ D[33] ^ D[30] ^ D[26] ^ D[25] ^ D[24] ^ D[23] ^ D[21] ^ D[19] ^ D[18] ^ D[15] ^ D[11] ^ D[10] ^ D[9] ^ D[8] ^ D[6] ^ D[4] ^ D[3] ^ D[0] ^ 0 ^ 0;
+	crc[1] = D[67] ^ D[66] ^ D[65] ^ D[63] ^ D[61] ^ D[60] ^ D[57] ^ D[53] ^ D[52] ^ D[51] ^ D[50] ^ D[48] ^ D[46] ^ D[45] ^ D[42] ^ D[38] ^ D[37] ^ D[36] ^ D[35] ^ D[33] ^ D[31] ^ D[30] ^ D[27] ^ D[23] ^ D[22] ^ D[21] ^ D[20] ^ D[18] ^ D[16] ^ D[15] ^ D[12] ^ D[8] ^ D[7] ^ D[6] ^ D[5] ^ D[3] ^ D[1] ^ D[0] ^ 0 ^ 0 ^ 0;
+	crc[2] = D[67] ^ D[66] ^ D[64] ^ D[62] ^ D[61] ^ D[58] ^ D[54] ^ D[53] ^ D[52] ^ D[51] ^ D[49] ^ D[47] ^ D[46] ^ D[43] ^ D[39] ^ D[38] ^ D[37] ^ D[36] ^ D[34] ^ D[32] ^ D[31] ^ D[28] ^ D[24] ^ D[23] ^ D[22] ^ D[21] ^ D[19] ^ D[17] ^ D[16] ^ D[13] ^ D[9] ^ D[8] ^ D[7] ^ D[6] ^ D[4] ^ D[2] ^ D[1] ^ 0 ^ 0 ^ 0;
+	crc[3] = D[67] ^ D[65] ^ D[63] ^ D[62] ^ D[59] ^ D[55] ^ D[54] ^ D[53] ^ D[52] ^ D[50] ^ D[48] ^ D[47] ^ D[44] ^ D[40] ^ D[39] ^ D[38] ^ D[37] ^ D[35] ^ D[33] ^ D[32] ^ D[29] ^ D[25] ^ D[24] ^ D[23] ^ D[22] ^ D[20] ^ D[18] ^ D[17] ^ D[14] ^ D[10] ^ D[9] ^ D[8] ^ D[7] ^ D[5] ^ D[3] ^ D[2] ^ 0 ^ 0;
+	
+	return crc;
+endfunction
+
+
+function logic [3:0] _crc3(input logic [36:0] D);
+
+	logic [2:0] crc;
+
+	crc[0] = D[35] ^ D[32] ^ D[31] ^ D[30] ^ D[28] ^ D[25] ^ D[24] ^ D[23] ^ D[21] ^ D[18] ^ D[17] ^ D[16] ^ D[14] ^ D[11] ^ D[10] ^ D[9] ^ D[7] ^ D[4] ^ D[3] ^ D[2] ^ D[0] ^ 0;
+	crc[1] = D[36] ^ D[35] ^ D[33] ^ D[30] ^ D[29] ^ D[28] ^ D[26] ^ D[23] ^ D[22] ^ D[21] ^ D[19] ^ D[16] ^ D[15] ^ D[14] ^ D[12] ^ D[9] ^ D[8] ^ D[7] ^ D[5] ^ D[2] ^ D[1] ^ D[0] ^ 0 ^ 0;
+	crc[2] = D[36] ^ D[34] ^ D[31] ^ D[30] ^ D[29] ^ D[27] ^ D[24] ^ D[23] ^ D[22] ^ D[20] ^ D[17] ^ D[16] ^ D[15] ^ D[13] ^ D[10] ^ D[9] ^ D[8] ^ D[6] ^ D[3] ^ D[2] ^ D[1] ^ 0 ^ 0;
+	
+	return crc;
+endfunction
+
+
+/**
+ * Internal ALU tasks
+ */
+
+task _tx_byte(input logic [7:0] data, input cmd_t tx_type);
+
+	/* START bit */
+	@(negedge clk);
+	sin = 1'b0;
+	
+	/* TYPE bit */
+	@(negedge clk);  
+	if(tx_type)
+		sin = 1'b1;
+	else
+		sin = 1'b0;
+	
+	/* DATA bits */
+	for(int i = 7;i >= 0; i--) begin
+		@(negedge clk);
+		sin = data[i];
+	end
+	
+	/* STOP bit */
+	@(negedge clk);
+	sin = 1'b1;
+endtask
+
+
+task _rx_byte(output logic [7:0] data, output cmd_t rx_type);
+	
+	/* START and TYPE bits */
+	repeat (2) @(negedge clk);
+	if(sout)
+		rx_type = CTL;
+	else
+		rx_type = DATA;
+	
+	/* DATA bits */
+	for(int i = 7;i >= 0; i--) begin
+		@(negedge clk);
+		data[i] = sout;
+	end
+	
+	/* STOP bit */
+	@(negedge clk);
+endtask
+
+
+task _rx_rsp(output rsp_t rsp_packet);
+	
+	logic rsp_type;
+	logic [7:0] data;
+	
+	@(negedge sout);
+	_rx_byte(data, rsp_type);
+
+	if(rsp_type) begin
+		rsp_packet.data = 32'h00000000;
+		rsp_packet.flags = data[6:1];
+		assert(data[0] == 1'b1) else $error("Invalid parity of ctl");
+	end
+	else begin
+		rsp_packet.data[31:24] = data;
+		_rx_byte(rsp_packet.data[23:16], rsp_type);
+		_rx_byte(rsp_packet.data[15:8], rsp_type);
+		_rx_byte(rsp_packet.data[7:0], rsp_type);
+		_rx_byte(data, rsp_type);
+		rsp_packet.flags = {2'b00, data[6:3]};
+		assert (data[2:0] == _crc3({rsp_packet.data, 1'b0, data[6:3]})) else $error("Invalid CRC3 of data");
+	end
+endtask
+
+task _alu_op(
+	input logic signed [31:0] A, 
+	input logic signed [31:0] B, 
+	input logic [2:0] OP, 
+	input logic [2:0] ERROR,
+	output rsp_t rsp_packet);
+
+	logic [3:0] crc;
+	
+	_tx_byte(B[31:24], DATA);
+	_tx_byte(B[23:16], DATA);
+	_tx_byte(B[15:8], DATA);
+	if(ERROR == F_ERRDATA) 
+		_tx_byte(B[7:0], CTL);
+	else
+		_tx_byte(B[7:0], DATA);
+	
+	_tx_byte(A[31:24], DATA);
+	_tx_byte(A[23:16], DATA);
+	_tx_byte(A[15:8], DATA);
+	_tx_byte(A[7:0], DATA);
+
+	crc = _crc4({B,A,1'b1,OP});
+	
+	if(ERROR == F_ERRCRC)
+		_tx_byte({1'b0, OP, crc[3:1],~crc[0]}, CTL);
+	else
+		_tx_byte({1'b0, OP, crc}, CTL);
+	
+	rsp_packet.data = 32'h00000000;
+	rsp_packet.flags = 6'b000000;
+	_rx_rsp(rsp_packet);
+endtask
+
+
+/**
+ * ALU external tasks and functions
+ */
+
+task rst();
+	sin = 1'b1;
+	rst_n = 1'b0;
+	
+	@(negedge clk);    
+    rst_n = 1'b1;
+endtask
+
+task op(
+	input logic signed [31:0] A, 
+	input logic signed [31:0] B, 
+	input logic [2:0] OP, 
+	input logic [2:0] ERROR,
+	output rsp_t rsp_packet);
+	
+	_alu_op(A, B, OP, ERROR, rsp_packet);
+endtask
+
+/**
+ * Clock generation
+ */
+ 
+initial begin
+	clk = 1'b1;
+	
+	forever
+		clk = #2.5 ~clk;
+end
+
+endinterface
---
+++ Lab02/alu_tb.sv
@@ -0,0 +1,344 @@
+/******************************************************************************
+* Created by dstankiewicz on Oct 17, 2021
+*******************************************************************************/
+`timescale 1ns / 1ps
+
+import alu_pkg::*;
+
+module alu_tb();
+
+/**
+ * Local variables and signals
+ */
+
+logic clk;
+logic rst_n;
+logic sin;
+logic sout;
+logic signed [31:0] A, B;
+
+logic [4:0] err_gen;
+logic [2:0] err_in;
+logic [1:0] op_gen;
+logic [2:0] op_in;
+logic [3:0] data_gen;
+logic rep_op;
+	
+rsp_t RSP;
+	
+	
+/**
+ * Interfaces instantiation
+ */ 
+ 
+alu_if alu_if(
+	.clk,
+	.rst_n,
+	.sin,
+	.sout
+);
+	
+	
+/**
+ * Submodules placement
+ */ 
+
+mtm_Alu mtm_Alu(
+	.clk,
+	.rst_n,
+	.sin,
+	.sout
+);
+	
+	
+/**
+ * Test verifier
+ */
+
+function logic verify_result(
+	input logic signed [31:0] A, 
+	input logic signed [31:0] B, 
+	input logic [2:0] OP, 
+	input logic [2:0] ERROR,
+	input rsp_t RSP);
+	
+	logic signed [31:0] RESULT;
+	logic [32:0] RESULT_CARRY;
+	logic [3:0] ALU_FLAGS;
+	
+	ALU_FLAGS = F_NONE;
+	
+	case(OP)
+		AND_OP:	begin
+			RESULT = B & A;
+			RESULT_CARRY = 33'h000000000;
+		end
+		OR_OP:	begin
+			RESULT = B | A;
+			RESULT_CARRY = 33'h000000000;
+		end
+		ADD_OP: begin
+			RESULT = B + A;
+			RESULT_CARRY = {1'b0, B} + {1'b0, A};
+			
+			if((A[31] == 1'b0 && B[31] == 1'b0 && RESULT[31] == 1'b1) || 
+				(A[31] == 1'b1 && B[31] == 1'b1 && RESULT[31] == 1'b0))
+				ALU_FLAGS |= F_OVFL;
+		end
+		SUB_OP: begin
+			RESULT = B - A;
+			RESULT_CARRY = {1'b0, B} - {1'b0, A};
+			
+			if((A[31] == 1'b1 && B[31] == 1'b0 && RESULT[31] == 1'b1) || 
+				(A[31] == 1'b0 && B[31] == 1'b1 && RESULT[31] == 1'b0))
+				ALU_FLAGS |= F_OVFL;
+		end
+		default: begin
+			RESULT = 32'h00000000;
+			RESULT_CARRY = 33'h000000000;
+		end
+	endcase
+	
+	if(RESULT_CARRY[32] == 1'b1) 
+		ALU_FLAGS |= F_CARRY;
+	
+	if(RESULT < 0)
+		ALU_FLAGS |= F_NEG;
+	
+	if(RESULT == 0)
+		ALU_FLAGS |= F_ZERO;
+	
+`ifdef DEBUG
+	$display("|         OP: %03b", OP);
+	$display("|          B: 0x%08h", B);
+	$display("|          A: 0x%08h", A);
+	$display("|          C: 0x%08h", RSP.data);
+	$display("|      FLAGS: %06b", RSP.flags);
+	$display("|      C_EXP: 0x%08h", RESULT);
+	
+	if(ERROR)
+		$display("|      ERROR: %06b", {ERROR, ERROR});
+	else
+		$display("|  FLAGS_EXP: %06b", ALU_FLAGS);
+`endif
+	
+	if((RSP.data == RESULT && RSP.flags[3:0] == ALU_FLAGS) || 
+		RSP.flags[5:3] == ERROR && ERROR != F_ERRNONE) begin
+		$display("TEST PASSED\n");
+		return 1'b0;
+	end
+	else begin
+		$display("TEST FAILED\n");
+		return 1'b1;
+	end
+endfunction
+
+
+/**
+ * Data generator
+ */
+ 
+function logic signed [31:0] gen_data();
+	data_gen = $urandom() % 16;
+	case (data_gen)
+		0: return 32'h00000000;
+		1: return 32'hFFFFFFFF;
+		2: return 32'h80000000;
+		3: return 32'h7FFFFFFF;
+		default: return $random;
+	endcase
+endfunction
+
+
+/**
+ * Error generator
+ */
+ 
+function logic [2:0] gen_error();
+	err_gen = $urandom() % 64;
+	case (err_gen)
+		0: return F_ERRCRC;
+		1: return F_ERRDATA;
+		2: return F_ERROP;
+		default: return F_ERRNONE;
+	endcase
+endfunction
+
+
+/**
+ * Operation generator
+ */
+ 
+function logic [2:0] gen_op(input logic [2:0] err_in);
+	op_gen = $urandom() % 4;
+	if (err_in == F_ERROP) begin
+		case (op_gen)
+			0: return 3'b010;
+			1: return 3'b011;
+			2: return 3'b110;
+			3: return 3'b111;
+		endcase
+	end
+	else begin
+		case (op_gen)
+			0: return AND_OP;
+			1: return OR_OP;
+			2: return ADD_OP;
+			3: return SUB_OP;
+		endcase
+	end
+endfunction
+
+/**
+ * Coverage
+ */
+ 
+covergroup op_cov;
+	
+	option.name = "cg_op_cov";
+	
+	coverpoint op_in {
+		
+		bins A1_basic_op = {AND_OP, OR_OP, ADD_OP, SUB_OP};
+		
+		bins A1_error_op = {3'b010, 3'b011, 3'b110, 3'b111};
+		
+		bins A2_repeated_op = ({AND_OP, OR_OP, ADD_OP, SUB_OP} [* 2]);
+	}
+	
+endgroup
+
+covergroup extreme_val_on_ops;
+	
+	option.name = "cg_extreme_val_on_ops";
+	
+	all_ops : coverpoint op_in {
+        ignore_bins not_supported_ops = {3'b010, 3'b011, 3'b110, 3'b111};
+	}
+	
+	a_arg: coverpoint A {
+        bins zeros = {32'h00000000};
+		bins min = {32'h80000000};
+        bins others= {[32'h00000001:32'h7FFFFFFE], [32'h80000001:32'hFFFFFFFE]};
+		bins max  = {32'h7FFFFFFF};
+        bins ones  = {32'hFFFFFFFF};
+    }
+
+    b_arg: coverpoint B {
+        bins zeros = {32'h00000000};
+		bins min = {32'h80000000};
+        bins others= {[32'h00000001:32'h7FFFFFFE], [32'h80000001:32'hFFFFFFFE]};
+		bins max  = {32'h7FFFFFFF};
+        bins ones  = {32'hFFFFFFFF};
+    }
+    
+    B_op_extreme_values: cross a_arg, b_arg, all_ops {
+
+        // #B1 simulate zero input for supported operations
+
+    	bins B1_and_zeros = binsof (all_ops) intersect {AND_OP} &&
+        	(binsof (a_arg.zeros) || binsof (b_arg.zeros));
+
+        bins B1_or_zeros = binsof (all_ops) intersect {OR_OP} &&
+        	(binsof (a_arg.zeros) || binsof (b_arg.zeros));
+
+        bins B1_add_zeros = binsof (all_ops) intersect {ADD_OP} &&
+        	(binsof (a_arg.zeros) || binsof (b_arg.zeros));
+
+        bins B1_sub_zeros = binsof (all_ops) intersect {SUB_OP} &&
+        	(binsof (a_arg.zeros) || binsof (b_arg.zeros));
+
+        // #B2 simulate ones input for supported operations
+
+        bins B2_and_ones = binsof (all_ops) intersect {AND_OP} &&
+        	(binsof (a_arg.ones) || binsof (b_arg.ones));
+
+        bins B2_or_ones = binsof (all_ops) intersect {OR_OP} &&
+        	(binsof (a_arg.ones) || binsof (b_arg.ones));
+
+        bins B2_add_ones = binsof (all_ops) intersect {ADD_OP} &&
+        	(binsof (a_arg.ones) || binsof (b_arg.ones));
+
+        bins B2_sub_ones = binsof (all_ops) intersect {SUB_OP} &&
+        	(binsof (a_arg.ones) || binsof (b_arg.ones));
+	    
+	    // #B3 simulate max input for supported operations
+
+        bins B3_and_max = binsof (all_ops) intersect {AND_OP} &&
+        	(binsof (a_arg.max) || binsof (b_arg.max));
+
+        bins B3_or_max = binsof (all_ops) intersect {OR_OP} &&
+        	(binsof (a_arg.max) || binsof (b_arg.max));
+
+        bins B3_add_max = binsof (all_ops) intersect {ADD_OP} &&
+        	(binsof (a_arg.max) || binsof (b_arg.max));
+
+        bins B3_sub_max = binsof (all_ops) intersect {SUB_OP} &&
+        	(binsof (a_arg.max) || binsof (b_arg.max));
+	    
+	    // #B4 simulate min input for supported operations
+
+        bins B2_and_min = binsof (all_ops) intersect {AND_OP} &&
+        	(binsof (a_arg.min) || binsof (b_arg.min));
+
+        bins B2_or_min = binsof (all_ops) intersect {OR_OP} &&
+        	(binsof (a_arg.min) || binsof (b_arg.min));
+
+        bins B2_add_min = binsof (all_ops) intersect {ADD_OP} &&
+        	(binsof (a_arg.min) || binsof (b_arg.min));
+
+        bins B2_sub_min = binsof (all_ops) intersect {SUB_OP} &&
+        	(binsof (a_arg.min) || binsof (b_arg.min));
+
+        ignore_bins others_only =
+        	binsof(a_arg.others) && binsof(a_arg.others);
+    }
+	
+endgroup
+
+op_cov 				operation_cov;
+extreme_val_on_ops 	extreme_val_cov;
+
+initial begin : coverage
+    operation_cov = new();
+    extreme_val_cov = new();
+    forever begin : sample_cov
+        @(posedge clk);
+        if(!rst_n) begin
+            operation_cov.sample();
+            extreme_val_cov.sample();
+        end
+    end
+end : coverage
+
+/**
+ * Tester
+ */
+
+initial begin : tester
+	alu_if.rst();
+	repeat (10000) begin
+		rep_op = ($urandom() % 32 == 0) ? 1'b1 : 1'b0;
+		
+		err_in = gen_error();
+		op_in = gen_op(err_in);
+		A = gen_data();
+		B = gen_data();
+		
+		alu_if.op(A, B, op_in, err_in, RSP);
+		assert(verify_result(A, B, op_in, err_in, RSP) == 1'b0);
+		
+		if(rep_op == 1'b1) begin
+`ifdef DEBUG
+			$display("|  Repeated operation");
+`endif
+			alu_if.op(A, B, op_in, err_in, RSP);
+			assert(verify_result(A, B, op_in, err_in, RSP) == 1'b0);
+		end
+	end
+	
+	repeat (10) @(negedge clk);  
+	$finish();
+end : tester
+
+endmodule
