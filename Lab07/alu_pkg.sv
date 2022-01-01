6f14eb48688b346195ab44d89f33f67f05d81882
Daniel Stankiewicz <traitorablepl@gmail.com>
2022-01-02 00:16:51 +0100

    Added custiomized debug prints coloring

--- Lab07/alu_pkg.sv
+++ Lab07/alu_pkg.sv
@@ -49,7 +49,37 @@ package alu_pkg;
 		bit [2:0] ERROR;
 		bit RST;
 		exp_result_t EXP_RESULT;
-	} cmd_pack_t;
+    } cmd_pack_t;
+    
+    typedef enum {
+        COLOR_BOLD_BLACK_ON_GREEN,
+        COLOR_BOLD_BLACK_ON_RED,
+        COLOR_BOLD_BLACK_ON_YELLOW,
+        COLOR_BOLD_BLUE_ON_WHITE,
+        COLOR_BLUE_ON_WHITE,
+        COLOR_DEFAULT
+    } print_color_t;
+    
+////////////////////////////////////////
+// ALU tasks and functions
+////////////////////////////////////////
+
+function void set_print_color(print_color_t c);
+    string ctl;
+    case(c)
+        COLOR_BOLD_BLACK_ON_GREEN : ctl = "\033\[1;30m\033\[102m";
+        COLOR_BOLD_BLACK_ON_RED : ctl = "\033\[1;30m\033\[101m";
+        COLOR_BOLD_BLACK_ON_YELLOW : ctl = "\033\[1;30m\033\[103m";
+        COLOR_BOLD_BLUE_ON_WHITE : ctl = "\033\[1;34m\033\[107m";
+        COLOR_BLUE_ON_WHITE : ctl = "\033\[0;34m\033\[107m";
+        COLOR_DEFAULT : ctl = "\033\[0m\n";
+        default : begin
+            $error("set_print_color: bad_argument");
+            ctl = "";
+        end
+    endcase
+    $write(ctl);
+endfunction
 
 ////////////////////////////////////////
 // ALU includes
